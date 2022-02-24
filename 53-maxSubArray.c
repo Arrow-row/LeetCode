112655a7439d2a004b8a1b073d8f702199a1afdc
Arrow <1512803831@qq.com>
2022-02-24 15:27:24 +0800

    53

--- 53-maxSubArray.c
+++ 53-maxSubArray.c
@@ -1,6 +1,35 @@
+#include <stdio.h>
+
+//动态规划方法
+int maxSubArray(int* nums, int numsSize){
+    int dp[numsSize];      //结果数组长度默认为求解数组长度，空间复杂度O(n)
+        dp[0]=nums[0];
+    for(int i=1;i<numsSize;i++){    //只需要遍历一次，时间复杂度O(n)
+        if(dp[i-1]<=0)
+            dp[i]=nums[i];
+        else
+            dp[i]=nums[i]+dp[i-1];
+        }
+    
+    for(int i=1;i<numsSize;i++){    //求结果数组dp[numsSize]中的最大值
+        //printf("%d ",dp[i] );
+        if(dp[i]>dp[0])
+            dp[0]=dp[i];
+    }
+    printf("%d\n", dp[0]); 
+}
+
+int main(int argc, char const *argv[])
+{
+    int nums[]={-2,1,-3,4,-1,2,1,-5,4},numsSize=9;
+    maxSubArray(nums,numsSize);
+    return 0;
+}
+
 
 
 //暴力解法超出时间限制
+/*
 int maxSubArray(int* nums, int numsSize){
     int cur,max=nums[0];
     if (numsSize==1)
@@ -21,3 +50,4 @@ int maxSubArray(int* nums, int numsSize){
     }
     return max;
 }
+*/
